cb4bbdb7354bf0b3b4ed9ae2cbe59d9c65fe20d5
milind69 <kulkarni.milindp@gmail.com>
2026-03-03 18:42:25 -0600

    restore back

--- cloudrun.tf
+++ cloudrun.tf
@@ -1,107 +1,102 @@
-# resource "google_cloud_run_v2_service" "my-fastapi-app" {
-#   name                = "${var.app_name}-app"
-#   location            = "us-central1"
-#   deletion_protection = false
-#   #ingress = "INGRESS_TRAFFIC_ALL"
-#   ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
-#   template {
-#     containers {
-#       name  = var.app_name
-#       image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.my-aft-repo.repository_id}/my-fastapi-app:latest"
-#       ports {
-#         container_port = 8000
-#       }
+resource "google_cloud_run_v2_service" "my-fastapi-app" {
+  name                = "${var.app_name}-app"
+  location            = "us-central1"
+  deletion_protection = false
+  #ingress = "INGRESS_TRAFFIC_ALL"
+  ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+  template {
+    containers {
+      name  = var.app_name
+      image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.my-aft-repo.repository_id}/my-fastapi-app:latest"
+      ports {
+        container_port = 8000
+      }
 
-#       resources {
-#         limits = {
-#           cpu    = "1"
-#           memory = "512Mi"
-#         }
-#       }
-#     }
-#     scaling {
-#       min_instance_count = 0
-#       max_instance_count = 2
-#     }
-#   }
-# }
-
-
-# resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-kooltech" {
-#   name     = google_cloud_run_v2_service.my-fastapi-app.name
-#   location = var.region
-#   project  = var.project_id
-#   role     = "roles/run.invoker"
-#   member   = "user:milind@kooltech.xyz"
-# }
-
-# # to use extenal account 
-# resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-external-viewer" {
-#   name     = google_cloud_run_v2_service.my-fastapi-app.name
-#   location = var.region
-#   project  = var.project_id
-#   role     = "roles/run.viewer"
-#   member   = "user:kulkarni.milind@gmail.com"
-# }
-
-# resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-external-invoker" {
-#   name     = google_cloud_run_v2_service.my-fastapi-app.name
-#   location = var.region
-#   project  = var.project_id
-#   role     = "roles/run.invoker"
-#   member   = "user:kulkarni.milind@gmail.com"
-# }
-
-# resource "google_service_account" "apigee-cloud-runnersa" {
-#   account_id   = "apigee-cloud-runnersa"
-#   display_name = "Apigee cloud runner"
-#   project      = var.project_id
-# }
-
-# # To grant this access you need to grant the terraform id a orgpolicy role 
-
-# # gcloud organizations add-iam-policy-binding 1069851491353 \
-# #   --member="user:terraformid@org.com" \
-# # --role="roles/orgpolicy.policyAdmin"
-
-# resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-apigee-cloud-runner" {
-#   name     = google_cloud_run_v2_service.my-fastapi-app.name
-#   location = var.region
-#   project  = var.project_id
-#   role     = "roles/run.invoker"
-#   member   = "serviceAccount:${google_service_account.apigee-cloud-runnersa.email}"
-# }
+      resources {
+        limits = {
+          cpu    = "1"
+          memory = "512Mi"
+        }
+      }
+    }
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 2
+    }
+  }
+}
 
 
-# resource "google_compute_region_network_endpoint_group" "cloud_run_neg" {
-#   name                  = "${var.app_name}-neg"
-#   project               = var.project_id
-#   region                = var.region
-#   network_endpoint_type = "SERVERLESS"
+resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-kooltech" {
+  name     = google_cloud_run_v2_service.my-fastapi-app.name
+  location = var.region
+  project  = var.project_id
+  role     = "roles/run.invoker"
+  member   = "user:milind@kooltech.xyz"
+}
 
-#   cloud_run {
-#     service = google_cloud_run_v2_service.my-fastapi-app.name
-#   }
-# }
+# to use extenal account 
+resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-external-viewer" {
+  name     = google_cloud_run_v2_service.my-fastapi-app.name
+  location = var.region
+  project  = var.project_id
+  role     = "roles/run.viewer"
+  member   = "user:kulkarni.milind@gmail.com"
+}
+
+resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-external-invoker" {
+  name     = google_cloud_run_v2_service.my-fastapi-app.name
+  location = var.region
+  project  = var.project_id
+  role     = "roles/run.invoker"
+  member   = "user:kulkarni.milind@gmail.com"
+}
+
+resource "google_service_account" "apigee-cloud-runnersa" {
+  account_id   = "apigee-cloud-runnersa"
+  display_name = "Apigee cloud runner"
+  project      = var.project_id
+}
+
+# To grant this access you need to grant the terraform id a orgpolicy role 
+
+# gcloud organizations add-iam-policy-binding 1069851491353 \
+#   --member="user:terraformid@org.com" \
+# --role="roles/orgpolicy.policyAdmin"
+
+resource "google_cloud_run_v2_service_iam_member" "my-fastapi-app-apigee-cloud-runner" {
+  name     = google_cloud_run_v2_service.my-fastapi-app.name
+  location = var.region
+  project  = var.project_id
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.apigee-cloud-runnersa.email}"
+}
 
 
+resource "google_compute_region_network_endpoint_group" "cloud_run_neg" {
+  name                  = "${var.app_name}-neg"
+  project               = var.project_id
+  region                = var.region
+  network_endpoint_type = "SERVERLESS"
 
-# resource "google_compute_region_backend_service" "cloud_run_backend_service" {
-#   name                            = "${var.app_name}-backend"
-#   project                         = var.project_id
-#   region                          = var.region
-#   protocol                        = "HTTPS"
-#   connection_draining_timeout_sec = 10
-#   #load_balancing_scheme           = "EXTERNAL_MANAGED"
-#   load_balancing_scheme = "INTERNAL_MANAGED"
-#   timeout_sec           = 30
-#   backend {
-#     group           = google_compute_region_network_endpoint_group.cloud_run_neg.id
-#     capacity_scaler = 1.0
-#   }
-# }
+  cloud_run {
+    service = google_cloud_run_v2_service.my-fastapi-app.name
+  }
+}
 
 
 
-
-
+resource "google_compute_region_backend_service" "cloud_run_backend_service" {
+  name                            = "${var.app_name}-backend"
+  project                         = var.project_id
+  region                          = var.region
+  protocol                        = "HTTPS"
+  connection_draining_timeout_sec = 10
+  #load_balancing_scheme           = "EXTERNAL_MANAGED"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  timeout_sec           = 30
+  backend {
+    group           = google_compute_region_network_endpoint_group.cloud_run_neg.id
+    capacity_scaler = 1.0
+  }
+}
